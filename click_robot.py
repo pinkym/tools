06b0f71c9c11aa502e00b841da77676ee0ff58a3
Jiang <jiangmm.fnst@fujitsu.com>
2018-09-21 20:39:43 +0800

    add some features
    1.add time info into log
    2.set robot's work time
    
    Signed-off-by: Jiang <jiangmm.fnst@fujitsu.com>

--- click_robot.py
+++ click_robot.py
@@ -1,6 +1,9 @@
 import win32api, win32con
 
 import time
+import datetime
+
+time_format = "%Y-%m-%d %H:%M:%S"
 
 
 def click(x, y):
@@ -13,19 +16,41 @@ def get_pos():
     return win32api.GetCursorPos()
 
 
+def time_to_str(time_v="now"):
+    if time_v == "now":
+        now_time_struct = time.localtime(time.mktime(time.localtime()))
+        return time.strftime(time_format, now_time_struct)
+    else:
+        now_time_struct = time.localtime(time_v)
+        return time.strftime(time_format, now_time_struct)
+
+
+def time_value(time_str="now"):
+    if time_str == "now":
+        return time.mktime(time.localtime())
+    else:
+        time_str = time.strftime(time_format.split(' ')[0] + " ", datetime.date.today().timetuple()) + time_str
+        return time.mktime(time.strptime(time_str, time_format))
+
+
+start_time = "09:00:00"
+end_time = "20:36:00"
 last_pos = pos = get_pos()
 while True:
-    time.sleep(120)
-    pos = get_pos()
-    if last_pos == pos:
-        print("Last pos %s ; At this moment pos %s; Robot worked." % (last_pos, pos))
-        click(10, 10)
-        last_pos = get_pos()
-    else:
-        print("Last pos %s ; At this moment pos %s; Robot doesn't have to work." % (last_pos, pos))
-        last_pos = pos
-
+    now = time_value()
+    if time_value(start_time) < now < time_value(end_time):
+        time.sleep(20)
+        pos = get_pos()
+        if last_pos == pos:
+            print("%s: Last pos %s ; At this moment pos %s; Robot worked." % (time_to_str(now), last_pos, pos))
+            click(10, 10)
+            last_pos = get_pos()
+        else:
+            print("%s: Last pos %s ; At this moment pos %s; Robot doesn't have to work." %
+                  (time_to_str(now), last_pos, pos))
+            last_pos = pos
 
 # add logic for break.
 # add logic for keyboard status check
 # add time log
+
