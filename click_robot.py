98a31ea27bdf3a4e16009ecc2f43e3a169bdccb2
mmm <1320381667@qq.com>
2018-09-26 10:28:59 +0800

    apply oop
    add class ClickRobot
    add break time duration for robot
    
    Signed-off-by: mmm <1320381667@qq.com>

--- click_robot.py
+++ click_robot.py
@@ -5,24 +5,56 @@ import datetime
 import configparser
 import os
 
-
 config = configparser.ConfigParser()
 conf_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf', 'setting')
 config.read(conf_dir)
 
 time_format = config.get("log", "time_format")
-start_time = config.get("robot", "start_time")
-end_time = config.get("robot", "end_time")
 
 
-def click(x, y):
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+class ClickRobot(object):
+    start_time = ""
+    end_time = ""
+    break_start = ""
+    break_end = ""
+    pos = ""
+    last_pos = ""
 
+    def __init__(self):
+        self.start_time = config.get("robot", "start_time")
+        self.end_time = config.get("robot", "end_time")
+        self.break_start = config.get("robot", "break_start")
+        self.break_end = config.get("robot", "break_end")
+        self.last_pos = self.pos = self.get_pos()
 
-def get_pos():
-    return win32api.GetCursorPos()
+    def click(self, x, y):
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+    def get_pos(self):
+        return win32api.GetCursorPos()
+
+    def run(self):
+        while True:
+            time.sleep(120)
+            now = time_value()
+            if time_value(self.start_time) < now < time_value(self.break_start) or time_value(
+                    self.break_end) < now < time_value(self.end_time):
+                self.pos = self.get_pos()
+                if self.last_pos == self.pos:
+                    print("%s: Last pos %s ; At this moment pos %s; Robot worked." %
+                          (time_to_str(now), self.last_pos, self.pos))
+                    self.click(10, 10)
+                    self.last_pos = self.get_pos()
+                else:
+                    print("%s: Last pos %s ; At this moment pos %s; Robot doesn't have to work." %
+                          (time_to_str(now), self.last_pos, self.pos))
+                    self.last_pos = self.pos
+            elif now < time_value(self.start_time) or now > time_value(self.end_time) or time_value(
+                    self.break_start) < now < time_value(self.break_end):
+                print("%s: Last pos %s ; At this moment pos %s; Robot is having a break." %
+                      (time_to_str(now), self.last_pos, self.pos))
 
 
 def time_to_str(time_v="now"):
@@ -30,6 +62,7 @@ def time_to_str(time_v="now"):
         now_time_struct = time.localtime(time.mktime(time.localtime()))
         return time.strftime(time_format, now_time_struct)
     else:
+        # TODO:time_v check
         now_time_struct = time.localtime(time_v)
         return time.strftime(time_format, now_time_struct)
 
@@ -38,30 +71,13 @@ def time_value(time_str="now"):
     if time_str == "now":
         return time.mktime(time.localtime())
     else:
+        # TODO:time_str check
         time_str = time.strftime(time_format.split(' ')[0] + " ", datetime.date.today().timetuple()) + time_str
         return time.mktime(time.strptime(time_str, time_format))
 
 
-last_pos = pos = get_pos()
-while True:
-    time.sleep(120)
-    now = time_value()
-    if time_value(start_time) < now < time_value(end_time):
-        pos = get_pos()
-        if last_pos == pos:
-            print("%s: Last pos %s ; At this moment pos %s; Robot worked." % (time_to_str(now), last_pos, pos))
-            click(10, 10)
-            last_pos = get_pos()
-        else:
-            print("%s: Last pos %s ; At this moment pos %s; Robot doesn't have to work." %
-                  (time_to_str(now), last_pos, pos))
-            last_pos = pos
-
-# add logic for break.
 # add logic for keyboard status check
-# config file management
 
-# class ClickRobot(object):
-#
-#
-#
+if __name__ == "__main__":
+    robot = ClickRobot()
+    robot.run()
